cc42e96e1c4eb77706cdf93463c155b03d00e723
ievgend2 <ievgend2@gmail.com>
2021-01-17 11:44:26 -0500

    Updates

---
+++ precache-manifest.9d91a173625e998f6a9e32f32b368341.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b04411c3fe44762c473bf1580c36455",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "7c5da293a8be7862043a",
+    "url": "/Tic-Tac-Toe/static/css/main.e04a0a15.chunk.css"
+  },
+  {
+    "revision": "47a1dab4e95c3106ff59",
+    "url": "/Tic-Tac-Toe/static/js/2.23514a74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac-Toe/static/js/2.23514a74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c5da293a8be7862043a",
+    "url": "/Tic-Tac-Toe/static/js/main.474c25ed.chunk.js"
+  },
+  {
+    "revision": "67245614e3d0dcb1dd84",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.5b3a8062.js"
+  },
+  {
+    "revision": "9f3d517d552dd4972fb1c706e99afa10",
+    "url": "/Tic-Tac-Toe/static/media/logo.9f3d517d.png"
+  }
+]);
